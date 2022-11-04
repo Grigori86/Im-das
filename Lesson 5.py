052577605d4d2dac14fc950ddabc43983d099eb9
Grigori86 <116946910+Grigori86@users.noreply.github.com>
2022-11-05 00:10:26 +0400

    Add files via upload

---
+++ Lesson 5.py
@@ -0,0 +1,57 @@
+# x = ("string")
+# length = len(x)
+# if  length > 2:
+#     if x[-3:] == 'ing':
+#       x += 'ly'
+#     else:
+#       x += 'ing'
+#     print(x)
+#
+#
+# user_input =input("Vorn e aveli hacheli achqerin? ")
+# print("Vorn e harmar is", user_input.upper())
+# print("Vorn e harmar is ", user_input.lower())
+# user_input = input("intejer")
+# user_input1= input("string")
+
+# string = input(("Enter your string"))
+# if(string == string[::-1]):
+#     print('palindrome')
+# else:
+#     print("No palindrome")
+# s = input()
+# print(len(s))
+# t = input()
+# number = int(t)
+# u = str(number)
+# print(s * 3)
+# print(s + ' ' + u)
+
+# my_list = [1, 3, -4, 6, -9]
+# my_list = [number for number in my_list if number >= 0]
+# print(my_list)
+
+
+
+# my_list = [1, 3, -4, 6, 14, 21, 33, -9]
+# my_list = [number for number in my_list if number % 2 ]
+# print(my_list)
+#
+# my_list = [1, 3, -4, 6, 14, 21, 33, -9]
+# print(max(my_list))
+
+# def multiplyList(myList):
+#     result = 1
+#     for x in myList:
+#         result = result * x
+#     return result
+#
+#
+# list2 = [5, 8, 3, 7]
+# print(multiplyList(list2))
+#
+# list = [12, 3, 20, 45, 8, 20, 6]
+# index = list.index(20)
+# list[index] = 200
+# print(list)
+# print(multiplyList())
