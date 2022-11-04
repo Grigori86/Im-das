0a10b40269bc234ce6a1acf14cb28e9040810472
Grigori86 <116946910+Grigori86@users.noreply.github.com>
2022-11-05 00:39:11 +0400

    Add files via upload

---
+++ tnayin8.py
@@ -0,0 +1,102 @@
+# -------------3-----------
+# n = int(input("Enter number:\t"))
+# count = 0
+# while (n > 0):
+#     count = count + 1
+#     n = n // 10
+# print("The number are:", count)
+
+# print(len(str(num)))
+
+# ---------------5--------
+# n = 5
+# for i in range(1, n + 1):
+#     for j in range(1, i + 1):
+#         print('*', end=' ')
+#     print("")
+#
+# # ----------------9------------
+# list = [5, -7, 65, 9, 2, 4, -85, -7, -87, -52, 32, 49]
+# positive = 0
+# negative = 0
+# for i in list:
+#     if (i > 0):
+#         positive = positive + 1
+#     else:
+#         negative = negative + 1
+# print("Positive number the list", positive)
+# print("Negative number the list", negative)
+#
+# # -----------10---------------
+# list = [52, 86, 7, 8]
+# print(list)
+# pos1,pos2 = 1, 2
+# list[pos1],list[pos2]=list[pos2],list[pos1]
+# print(list)
+
+# -----------1-----------
+# def perimetr(erk, layn):
+#     return 2 * (erk + layn)
+#
+#
+# erk = int(input("Erk:"))
+# layn = int(input("Layn:"))
+#
+# d = perimetr(erk, layn)
+# print("enter mak", d)
+# -----------2------------
+# def  of_the_square(length):
+#     return 4 * length
+#
+#
+# length = int(input("Enter number\t"))
+# a = of_the_square(length)
+# print(a)
+
+# -------------3-------------
+# def area_of_rectangle(length, width):
+#     return length * width
+#
+# length = int(input("Enter number length\t"))
+# width = int(input("Enter number width\t"))
+# z = area_of_rectangle(length, width)
+# print(z)
+
+# ----------4---------
+# def area_of_square(width):
+#     return width ** 2
+#
+# width = int(input("Enter\t"))
+# f = area_of_square(width)
+# print(f)
+#
+# ------------5-----------
+
+# def test(z):
+#     d = {'a': 0, 'b': 0}
+#     for i in z:
+#         if i.isupper():
+#             d["a"] += 1
+#         elif i.islower():
+#             d["b"] += 1
+#     else:
+#         pass
+#     print("Mec tarer", d["a"])
+#     print("poqr tarer", d["b"])
+#
+#
+# test('The quick Brown Fox')
+
+# -----------6---------
+# def u_list(numbers):
+#     y = []
+#     for item in numbers:
+#         if item not in y:
+#             y.append(item)
+#     return y
+#
+#
+# print(u_list([1, 2, 8, 3, 1, 2, 9, 15, 45, 52]))
+
+
+
