42947156484e24f1033f34729aa99a1536986cc4
Grigori86 <116946910+Grigori86@users.noreply.github.com>
2022-11-05 00:06:57 +0400

    Add files via upload

---
+++ dajan.py
@@ -0,0 +1,31 @@
+# for i in range(10,99):
+#       if i % 4 == 1:
+#             print(i)
+# for i in range(100, 601):
+#   if  i % 3 == 0 and i % 11 == 0 and i % 7 != 0:
+#                              print(i)
+# t = int(input("Yntrir taqutyuny\t"))
+# if t < 18:
+#     print("curt e")
+# if 24 > t > 18:
+#     print("Hacheli e")
+# if t > 24:
+#     print("Shog e")
+# i = int(input("Yntrir tivy\t"))
+# for i in range(10, 95):
+#     if i % 7 == 0:
+#         print(i)
+# i = input()
+# n = int(i)
+# r = 1
+# while n > 1 :
+#     r *= n
+#     n -= 1
+# print(r)
+# i = 7
+
+# while   i < 30:
+# if  i % 2 == 0:
+#     i += 1:
+#  print(i)
+
